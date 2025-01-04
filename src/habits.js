3f490ee0678c1bc2e7f773b65b2ab0675c592e24
SP <spsinghrathore1807@gmail.com>
2025-01-05 01:50:23 +0530

    the habits MVP is done!

--- src/habits.js
+++ src/habits.js
@@ -1,3 +1,20 @@
-document.addEventListener('DOMContenttLoaded', function(){
-   Commit();
+document.addEventListener('DOMContentLoaded', function habits(){
+   const li = document.querySelectorAll('.todo');
+li.forEach(task => {
+   task.addEventListener('click', function () {
+    task.style.display ='none';
+    done();
+   });
+ });
+ let count = 3;
+ function done() {
+   count--;
+   if(count === 0){
+     const text = document.querySelector('h4');
+     text.innerHTML = "YOU DID IT!";
+     text.style.backgroundColor = 'black';
+     text.style.color = 'white';
+     text.style.borderRadius = '26px';
+   }
+ }
 });
